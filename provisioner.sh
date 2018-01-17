923ec14135aab4158893f66c02f3bea7cbf8a134
Matthew Boyd <boydm@reed.edu>
2018-01-16 16:43:54 -0800

    Adding new packages to provisioner script

--- provisioner.sh
+++ provisioner.sh
@@ -12,7 +12,7 @@ set -e
 # end of this script.
 start_seconds="$(date +%s)"
 
-sudo apt update && sudo apt -y upgrade && sudo apt -y autoremove
+sudo apt update && sudo apt -y upgrade && sudo apt -y autoremove && sudo apt clean
 
 #install git
 sudo apt install -y git || echo "Clang installation failed"
@@ -24,12 +24,21 @@ sudo apt update
 sudo apt -y install gcc || echo "git installation failed" 
 echo 'GCC 7 installed successfully!'
 
+#install valgrind
+sudo apt install -y valgrind || echo "valgrind installation failed"
+echo 'valgrind installed successfully'
+
+#install flex and bison
+sudo apt install -y flex || echo "flex installation failed"
+sudo apt install -y bison || echo "bison installation failed"
+echo 'flex and bison installed successfully'
+
 #install latest clang version
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-add-repository "deb http://apt.llvm.org/zesty/ llvm-toolchain-zesty-5.0 main"
 sudo apt update
-sudo apt -y install clang-5.0 || echo "Clang installation failed" 
-sudo ln -s /usr/lib/llvm-5.0/bin/clang /usr/bin/clang
+sudo apt install -y clang-5.0 || echo "Clang installation failed" 
+sudo ln -sf /usr/lib/llvm-5.0/bin/clang /usr/bin/clang
 echo 'Clang-5 installed successfully!'
 
 #install SML/NJ
@@ -57,6 +66,24 @@ sudo apt install -y libssl-dev libcurl4-openssl-dev libxml2-dev || echo "tidyver
 echo 'dependencies for tidyverse installed successfully!'
 sudo R -e "install.packages('tidyverse', repos='https://ftp.osuosl.org/pub/cran/', dep = TRUE)"
 echo 'tidyverse successfully installed!'
+
+#install go
+sudo apt install -y golang-go || echo "go installation failed"
+echo 'go installed successfully'
+
+#install spim
+sudo apt install -y spim || echo "spim installation failed"
+echo 'spim installed successfully'
+
+#install sublime text
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+sudo apt update
+sudo apt install -y sublime-text-installer || echo "Sublime-text installation failed"
+echo 'sublime-text successfully installed!'
+
+#install chromium
+sudo apt install -y chromium-browser || echo "chromium installation failed"
+echo 'chromium successfully installed'
 
 # install ubuntu desktop
 sudo apt install -y ubuntu-desktop || echo "Desktop environment installation failed"
